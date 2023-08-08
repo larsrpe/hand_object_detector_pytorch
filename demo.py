ff6303e6e99acd710fcc36e9fba0da606cea3551
Lars Rønhaug Pettersen <80674086+larsrpe@users.noreply.github.com>
2023-08-08 10:14:41 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ from cv2 import imread
 from handobdet import HandObjectDetector, draw_detections
 
 def main():
-    input = imread("/Users/larsrpe/hand_object_detector_pytorch/images/5137daf7b29c5f4bc37322122803e597.jpg")
+    input = imread("assets/data_ego_frame/boardgame_848_sU8S98MT1Mo_00013957.png")
     detector = HandObjectDetector(input_format="BGR")
     dets=detector.detect(input,viz=False)
 if __name__ == "__main__":
